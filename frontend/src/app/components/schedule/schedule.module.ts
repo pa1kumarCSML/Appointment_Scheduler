bc727e7a4f86937364561617208cf5ca22988833
pavan kumar <dpavankumar8797@gmail.com>
2023-03-31 05:24:15 +0530

    frontend components,routes,forms

---
+++ frontend/src/app/components/schedule/schedule.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { ScheduleRoutingModule } from './schedule-routing.module';
+import { CommonModule } from '@angular/common';
+
+import { ScheduleComponent } from './schedule.component';
+import { ViewScheduleComponent } from './view-schedule/view-schedule.component';
+import { BookSlotComponent } from './book-slot/book-slot.component';
+import { AppointmentRequestsComponent } from './appointment-requests/appointment-requests.component';
+import { MyAppointmentsComponent } from './my-appointments/my-appointments.component';
+
+
+@NgModule({
+  declarations: [
+    ScheduleComponent,
+    ViewScheduleComponent,
+    BookSlotComponent,
+    AppointmentRequestsComponent,
+    MyAppointmentsComponent
+  ],
+  imports: [
+    CommonModule,
+    ScheduleRoutingModule
+  ]
+})
+export class ScheduleModule { }

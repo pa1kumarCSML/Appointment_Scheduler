c2302826b48c0009d5792787fcc2fa64d85ed746
pavan kumar <dpavankumar8797@gmail.com>
2023-04-14 14:41:19 +0530

    Update appointmentController.js

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -78,7 +78,7 @@ const updateAppointment = asyncHandler(async (req, res) => {
     if (req && req.user && req.user.Role != 4) {
         req.body.Status = 1
     }
-    if (await checkValidSlotOrNot(req.body.DateTime, req.body.Duration)) {
+    if (await checkValidSlotOrNot(req.body.DateTime, req.body.Duration, req.params.id)) {
         res.status(400)
         throw new Error("Slot is full in the requested time")
     }
@@ -127,7 +127,7 @@ const getAppointment = asyncHandler(async (req, res) => {
     })
 })
 
-const checkValidSlotOrNot = asyncHandler(async (datetime, duration) => {
+const checkValidSlotOrNot = asyncHandler(async (datetime, duration, id = null) => {
     date = datetime.split("T")[0]
     const regex = new RegExp(`^${date}`);
     const appointments = await Appointment.find(
@@ -142,14 +142,16 @@ const checkValidSlotOrNot = asyncHandler(async (datetime, duration) => {
     slotoverlap = false
     if (appointments.length) {
         appointments.forEach(appointment => {
-            start1 = moment(appointment.DateTime, 'YYYY-MM-DD HH:mm')
-            let endTime1 = start1.add(appointment.Duration, 'minutes').format('YYYY-MM-DD HH:mm')
-            s1 = new Date(appointment.DateTime)
-            e1 = new Date(endTime1)
-            if ((s2 <= s1 && e2 >= s1 && e2 <= e1) || (s2 <= e1 && e1 >= s2 && e1 <= e2) ||
-                (s2 >= s1 && e2 <= e1) || (s1 >= s2 && e1 <= e2)) {
-                slotoverlap = true
-                return
+            if (!id || id != appointment._id) {
+                start1 = moment(appointment.DateTime, 'YYYY-MM-DD HH:mm')
+                let endTime1 = start1.add(appointment.Duration, 'minutes').format('YYYY-MM-DD HH:mm')
+                s1 = new Date(appointment.DateTime)
+                e1 = new Date(endTime1)
+                if ((s2 <= s1 && e2 >= s1 && e2 <= e1) || (s2 <= e1 && e1 >= s2 && e1 <= e2) ||
+                    (s2 >= s1 && e2 <= e1) || (s1 >= s2 && e1 <= e2)) {
+                    slotoverlap = true
+                    return
+                }
             }
         })
     }
